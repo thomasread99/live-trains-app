e7f9a7bbb839e273837e0f1ebf2c7e50ded85566
Thomas Read <thomasread99@gmail.com>
2022-10-20 19:19:50 +0100

    feat: setup basic bottom tabs navigation

---
+++ navigation/MainNavigator.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+import SearchScreen from "../screens/SearchScreen";
+
+const Tab = createBottomTabNavigator();
+
+const MainNavigator = () => {
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={{
+					headerShown: false,
+					tabBarStyle: {
+						height: hp("8%"),
+					},
+					tabBarLabelStyle: {
+						fontSize: wp("4%"),
+					},
+				}}
+			>
+				<Tab.Screen
+					name="Search"
+					component={SearchScreen}
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Ionicons
+								name="search"
+								size={wp("8%")}
+								color={color}
+							/>
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Saved"
+					component={SearchScreen}
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Ionicons
+								name="star"
+								size={wp("8%")}
+								color={color}
+							/>
+						),
+					}}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default MainNavigator;
