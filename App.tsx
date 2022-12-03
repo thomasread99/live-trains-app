df78764990c65a515a398d73b683a5c8ab694609
Thomas Read <thomasread99@gmail.com>
2022-12-03 20:27:59 +0000

    feat: make status bar black

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 
 import store from "./store/store";
+import { StatusBar } from "react-native";
 
 export default function App() {
 	return (
 		<Provider store={store}>
+			<StatusBar backgroundColor="black"/>
 			<SafeAreaProvider>
 				<MainNavigator />
 			</SafeAreaProvider>
